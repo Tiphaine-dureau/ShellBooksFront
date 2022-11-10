831d0443393417c0ebe1ded00c720367e01ac5bd
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2022-11-10 16:07:05 +0100

    Added books component and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {BooksModule} from "./features/books/books.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BooksModule
   ],
   providers: [],
   bootstrap: [AppComponent]
